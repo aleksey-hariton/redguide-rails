9271ff7267e0242b74aaf47b88a3f8834020c011
Dzmitry Pasiukevich <dzmitry_pasiukevich@epam.com>
2017-05-27 18:59:14 +0300

    use find instead of find_by when searching for project config

--- app/controllers/api/prconfigs_controller.rb
+++ app/controllers/api/prconfigs_controller.rb
@@ -23,7 +23,7 @@ class PrconfigsController < Api::ApiController
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_prconfig
-        @prconfig = Prconfig.find_by(params[:id])
+        @prconfig = Prconfig.find(params[:id])
       end
 
       # Use callbacks to share common setup or constraints between actions.
