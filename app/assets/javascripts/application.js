7846f4e90af2ebe76c57d632d995a4b08118a7f2
Aleksey Hariton <aleksey_hariton1@epam.com>
2017-06-24 13:34:10 +0300

    Fixed issue with cookbooks build auto-update.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-$(document).ready(function() {
-    $(function() {
-        setTimeout(updateCookbookBuildStageStatus, 10000);
-    });
-});
-
-function updateCookbookBuildStageStatus () {
-    var changeset = $("#cookbooks-build").attr("data-id");
-    var project = $("#cookbooks-build").attr("data-project");
-    $.getScript("/projects/" + project +"/changesets/" + changeset + "/stage_status.js?stage=0")
-    setTimeout(updateCookbookBuildStageStatus, 10000);
-}
 
 function assignRefreshReaction(){
     // Add spin and disable 'play' button
