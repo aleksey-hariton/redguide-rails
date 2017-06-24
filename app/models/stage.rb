7979722a6ba660eb6a672c698a729b051f6a5b94
Aleksey Hariton <aleksey_hariton1@epam.com>
2017-06-24 17:17:21 +0300

    Added auto-update for stages

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -2,4 +2,7 @@ class Stage < ApplicationRecord
   belongs_to :project
   has_many :steps
 
+  def build_status(changeset)
+    StageBuild.find_by(changeset_id: changeset.id, stage_id: self.id)
+  end
 end
